217188a5b75a6f0101854f88e29f3131cfe5a94f
ceciliasanchez <cecielimacas2910@gmail.com>
2017-11-11 22:11:50 -0500

    cuarto commit

--- TareaHerencia1/src/principal/Principal.java
+++ TareaHerencia1/src/principal/Principal.java
@@ -23,12 +23,13 @@ public class Principal {
         Trabajador t = new Trabajador();
 
         Trabajador trab[] = new Trabajador[2];
+         EmpresaPrivada ep = new EmpresaPrivada("Soluciones Software", "SS's", "Loja", trab, 200000, 12);
 
         trab[0] = new Trabajador("Ana Luisa", "Velez Alcivar", 30, "12903939", 100, 40, 10, t.obtener_sueldo());
 
         trab[1] = new Trabajador("Mario Anibal", "Vela Narvaez", 35, "12903939", 100, 50, 10, t.obtener_sueldo());
 
-        EmpresaPrivada ep = new EmpresaPrivada("Soluciones Software", "SS's", "Loja", trab, 200000, 12);
+       
 
         System.out.println(ep);
 
